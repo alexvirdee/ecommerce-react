569faf8e67e39f0ec414d463a6c8028cff4a34ea
Alexinder Virdee <alex.virdee08@gmail.com>
2020-05-23 13:05:28 -0400

    Add firebase utility library

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCkDwb4HZhsOzL8xIxOC63EAgOWnMz22G8",
+    authDomain: "crwn-db-38cf0.firebaseapp.com",
+    databaseURL: "https://crwn-db-38cf0.firebaseio.com",
+    projectId: "crwn-db-38cf0",
+    storageBucket: "crwn-db-38cf0.appspot.com",
+    messagingSenderId: "146267780361",
+    appId: "1:146267780361:web:361424c600ea248feccfd8"
+  }
+
+firebase.initializeApp(config);
