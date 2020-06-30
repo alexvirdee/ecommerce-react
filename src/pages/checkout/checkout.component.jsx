d63295ce149c0ff2d1c28ad4d2be692aa42032c0
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-30 12:33:55 -0400

    Remove redux logger from production build

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,6 +36,10 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
         <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>*Please use the following test credit card for payments* 
+            <br />
+            4242 4242 4242 4242 - Exp: 06/20 - cvv 123
+        </div>
         <StripeCheckoutButton price={total} />
     </div>
 );
