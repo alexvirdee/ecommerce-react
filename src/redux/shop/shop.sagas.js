bd4c95fe0d6b46f330f8200ce504034494626630
Alexinder Virdee <alex.virdee08@gmail.com>
2020-07-21 15:38:07 -0400

    fetchCollectionsStart saga firing function

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -2,6 +2,7 @@ import { takeEvery } from 'redux-saga/effects';
 
 import ShopActionTypes from './shop.types';
 
+// Run sagas concurrently
 export function* fetchCollectionsAsync() {
     yield console.log('Firing');
 }
